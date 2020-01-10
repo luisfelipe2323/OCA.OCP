8e108a7f7da175dbf612c0c98e5bc3522abfb25e
LuiSamaniego <luis.felipe.2323@gmail.com>
2020-01-09 19:24:35 -0600

    primera respuesta

--- JavaSE8ProgrammerPracticeTest1/PartIOCA/src/Uno.java
+++ JavaSE8ProgrammerPracticeTest1/PartIOCA/src/Uno.java
@@ -1,12 +1,12 @@
 public class Uno {
 
     /*Which of the following method signatures is a valid declaration of an entry point in a Java application?
-
     A. public void main(String[] args)
     B. public static void main()
     C. private static void start(String[] mydata)
-    D. public static final void main(String[] mydata)*/
-    public static void main(String[] args) {
+   #D. public static final void main(String[] mydata)
+    */
+    public static final void main(String[] mydata) {
         System.out.println("Hola");
     }
 }
